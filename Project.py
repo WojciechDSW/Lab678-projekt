43b92301661bde136bb66c0c8c763b05a160d557
WojciechDSW <130590839+WojciechDSW@users.noreply.github.com>
2023-06-14 15:26:48 +0200

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+def load_xml(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        print(f"Dane z pliku {file_path} wczytane pomyślnie.")
+        return root
+    except ET.ParseError:
+        print(f"Błąd składni XML w pliku {file_path}.")
+        return None
+    except Exception as e:
+        print(f"Wystąpił problem podczas wczytywania pliku {file_path}: {str(e)}")
+        return None
+
+if __name__ == "__main__":
+    # Zamień 'input.xml' na prawdziwą ścieżkę do pliku.
+    root = load_xml('input.xml')
